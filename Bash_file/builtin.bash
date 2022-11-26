b73d47e888cf63e3f5080289e447d692ca88f036
jeonsion <a24349663@gmail.com>
2022-11-26 08:02:13 +0000

    study

--- Bash_file/builtin.bash
+++ Bash_file/builtin.bash
@@ -1,8 +1,12 @@
 #! /bin/bash    ->해당파일을 bash쉘로 실행시키겠다는 의미
 # builtin.bash
+# 사전정의 지역변수 17p 참고
 
 echo 이 스크립트 이름: $0
 echo 첫 번째 명령줄 인수: $1
 echo 모든 명렬줄 인수 : $*
 echo 이 스크립트를 실행하는 프로세스 번호: $$
 echo 명령줄 인수의 개수 : $#
+
+# $ chmod 55 builtin.bash
+# $ bash builtin.bash 로 실행해야 되네..
---
+++ Bash_file/state.bash
@@ -0,0 +1,8 @@
+#! /bin/bash
+#state.bash
+echo -n 현재 시간 :
+date
+echo -n 현재 사용자 :
+who
+echo -n 시스템 현재상황:
+uptime
